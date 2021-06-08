0a68d349e51cce8cceed71fc399fadfac726bddb
pehnovandrei <pehnovandrei@yandex.ru>
2021-06-08 12:24:18 +0300

    Update Life.java

--- game_of_life/src/ru/pas/Life.java
+++ game_of_life/src/ru/pas/Life.java
@@ -2,40 +2,65 @@ package ru.pas;
 
 import ru.pas.libs.Gui;
 import ru.pas.libs.Logic;
+import java.lang.Thread;
 
-public class Life{
+public class Life extends Thread {
     static boolean[][] oldGeneration;
+	static boolean newGeneration[][];
+	static int steps = 0;
+	static boolean stop = false;
 	static Logic logic;
 	static Gui gui;
-	public static void main(String[] arvs){
-		gui = new Gui(10);
+	static int currentGen = 0;
+	static int arrayLength = 10;
+	public static int next = 1;
+	public static void main(String[] args){
+		
+		if (args.length > 0){
+			try {
+				arrayLength = Integer.parseInt(args[0]);
+			}catch (NumberFormatException e){
+				arrayLength = 10;
+			}
+		}
+		gui = new Gui(arrayLength);
 		gui.run();
 		gui.setVisible(true);
-		logic = new Logic(10);
+		logic = new Logic(arrayLength);
 	}
 	public static void start(boolean[][] _firstGeneration){
 		oldGeneration = _firstGeneration;
-		boolean newGeneration[][] = new boolean[10][10];
-		boolean stop = false;
-		int steps = 0;
-		while (!stop)
-		{
-			steps ++;
-			newGeneration = logic.createNewGeneration(oldGeneration);
-			gui.setNewGeneration(newGeneration);
-			logic.saveGeneration(oldGeneration);
-			oldGeneration = newGeneration;
-			boolean compare = logic.compareGenerations(newGeneration);
-			if (compare){
-				stop = true;
-				gui.setTxt("Game Over: Life repeat self generation at " + steps + ".");
-				return;
+		newGeneration = new boolean[arrayLength][arrayLength];
+		Runnable task = () -> {
+			while (!stop){
+				try{
+					Thread.sleep(100);
+				if (currentGen < next){
+					steps ++;
+					gui.setTxt("Step: " + steps);
+					newGeneration = logic.createNewGeneration(oldGeneration);
+					gui.setNewGeneration(newGeneration);
+					logic.saveGeneration(oldGeneration);
+					oldGeneration = newGeneration;
+					boolean compare = logic.compareGenerations(newGeneration);
+					if (compare){
+						stop = true;
+						gui.setTxt("Game Over: Life repeat self generation at " + steps + ".");
+						return;
+					}
+					int liveCells = logic.cellsLiveCount(newGeneration);
+					if (liveCells == 0){
+						stop = true;
+						gui.setTxt("Game Over: All cells die for " + steps + " steps.");
+					}
+					currentGen = next;
+				}
+				}catch(InterruptedException e){
+					System.out.println("thread interrupted");
+				}
 			}
-			int liveCells = logic.cellsLiveCount(newGeneration);
-			if (liveCells == 0){
-				stop = true;
-				gui.setTxt("Game Over: All cells die for " + steps + " steps.");
-			}
-		}
+		};
+		Thread thread = new Thread(task);
+		thread.start();
 	}
 }
